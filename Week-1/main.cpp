4c103ead64c1c18025c2d3a2e1bb97b38e5f3aeb
Shiva-Ram20 <SHIVA.RAMDATH@JJAY.CUNY.EDU>
2026-01-29 11:47:15 -0500

    Add files via upload

---
+++ Week-1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout <<"Shiva Ramdath"<<endl;
+    cout << "CSCI 271"<<endl;
+    cout <<"I like playing videogames"<<endl;
+    
+return 0;
+}
+
